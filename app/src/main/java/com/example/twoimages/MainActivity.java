6e63a6133b46ee6635b8ed5cd7e345f6daa8be72
mitacsc <mayurkhalate63@gmail.com>
2024-08-14 08:42:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/twoimages/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.twoimages;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void light(View view) {
+        Toast.makeText(this, "Neon Painting", Toast.LENGTH_SHORT).show();
+    }
+
+    public void background(View view) {
+        Toast.makeText(this, "Scenery Of SeaShore", Toast.LENGTH_SHORT).show();
+    }
+}
